afbd31853acf02f6937d20142291e506275f0c98
jin.zhao <jin.zhao@xinda.im>
2018-04-23 17:20:15 +0800

    add docker file

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM jinwuzhao/emacs:latest
+
+ENV TERM=xterm-256color \
+    LANG=en_US.UTF-8 \
+    LANGUAGE=en_US.UTF-8 \
+    LC_CTYPE=en_US.UTF-8 \
+    LC_ALL=en_US.UTF-8 \
+    TIMEZONE=Asia/Shanghai
+
+WORKDIR /root
+
+VOLUME /root/Documents
+
+COPY ./.spacemacs .
+COPY ./install.sh .
+
+RUN chmod +x ./install.sh
+RUN ./install.sh
+RUN rm ./install.sh
+
+ENTRYPOINT ["launch.sh"]
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+apk --no-cache add musl-dev 
+apk --no-cache add gcc
+apk --no-cache add g++
+apk --no-cache add clang
+apk --no-cache add llvm
+apk --no-cache add make
+apk --no-cache add cmake
+apk --no-cache add openssl
+apk --no-cache add findutils
+
+cd /root/
+
+wget -c http://ftp.gnu.org/gnu/ncurses/ncurses-6.1.tar.gz
+tar -xf ncurses-6.1.tar.gz
+cd ncurses-6.1
+./configure --with-shared --without-debug --without-ada --enable-overwrite
+make
+make install
+cd ..
+rm -rf ncurses-6.1
+rm ncurses-6.1.tar.gz
+
+wget -c https://jaist.dl.sourceforge.net/project/cscope/cscope/15.8b/cscope-15.8b.tar.gz
+tar -xf cscope-15.8b.tar.gz
+cd cscope-15.8b
+./configure
+make
+make install
+cd ..
+rm -rf cscope-15.8b
+rm cscope-15.8b.tar.gz
+
+git clone https://github.com/syl20bnr/spacemacs.git /root/.emacs.d
+
+rm /root/install.sh
